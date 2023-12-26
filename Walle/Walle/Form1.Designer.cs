9e69ef5fdb54b9f0e195db72fa797101525f2ec1
amabe418 <112485984+amabe418@users.noreply.github.com>
2023-12-26 04:05:37 -0500

    Add files via upload
    creando interfaz de usuario

---
+++ Walle/Walle/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Walle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.customButton1 = new Walle.CustomButton();
+            this.SuspendLayout();
+            // 
+            // customButton1
+            // 
+            this.customButton1.AutoSize = true;
+            this.customButton1.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.customButton1.BackgroundColor = System.Drawing.Color.LightSeaGreen;
+            this.customButton1.BorderColor = System.Drawing.Color.LightSeaGreen;
+            this.customButton1.BorderRadius = 40;
+            this.customButton1.BorderSize = 5;
+            this.customButton1.FlatAppearance.BorderSize = 0;
+            this.customButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.customButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customButton1.ForeColor = System.Drawing.Color.White;
+            this.customButton1.Location = new System.Drawing.Point(106, 471);
+            this.customButton1.Name = "customButton1";
+            this.customButton1.Size = new System.Drawing.Size(250, 62);
+            this.customButton1.TabIndex = 1;
+            this.customButton1.Text = "Ejecutar";
+            this.customButton1.TextColor = System.Drawing.Color.White;
+            this.customButton1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(1136, 596);
+            this.Controls.Add(this.customButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private CustomButton customButton1;
+    }
+}
+
