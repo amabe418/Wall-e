f8dfaf82278017aee884df0ffba26f11edf17acb
amabe418 <112485984+amabe418@users.noreply.github.com>
2024-01-02 22:26:07 -0500

    Add files via upload

---
+++ Walle/Walle/GSharpInterpreter/GSharp/GSharpType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSharpInterpreter
+{
+    public enum GSharpType
+    {
+        NUMBER,
+        STRING,
+        MEASURE,
+        POINT,
+        LINE,
+        RAY,
+        SEGMENT,
+        CIRCLE,
+        ARC,
+        SEQUENCE,
+        UNDEFINED
+    }
+}
---
+++ Walle/Walle/GSharpInterpreter/Lexer/Token.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSharpInterpreter
+{
+    /// <summary>
+    /// Represents a token in the source code.
+    /// </summary>
+    public class Token
+    {
+        public TokenType Type { get; }  // The type of the token
+        public string Lexeme { get; }   // The lexeme of the token
+        public object Literal { get; }  // The literal value of the token
+        public int Line { get; }        // The line number where the token appears
+        public Token(TokenType type, string lexeme, Object literal, int line)
+        {
+            Type = type;
+            Lexeme = lexeme;
+            Literal = literal;
+            Line = line;
+        }
+
+        public override string ToString()
+        {
+            return Type + " " + Lexeme + " " + Literal;
+        }
+    }
+}
